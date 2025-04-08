58700b606562751468bb5b05541d08f94a72cf8f
Napoknot21 <napoknot21@protonmail.com>
2025-04-08 14:47:01 +0200

    Add of first version of VGG implementation

---
+++ main.py
@@ -0,0 +1,154 @@
+from torchvision.datasets import CIFAR10
+import numpy as np
+import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data.dataloader import DataLoader
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name='VGG11'):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+            else:
+                layers.extend([
+                    nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                    nn.BatchNorm2d(x),
+                    nn.ReLU(inplace=True)
+                ])
+                in_channels = x
+        layers.append(nn.AvgPool2d(kernel_size=1, stride=1))
+        return nn.Sequential(*layers)
+
+
+normalize_scratch = transforms.Normalize(
+    (0.4914, 0.4822, 0.4465),
+    (0.2023, 0.1994, 0.2010)
+)
+
+# Transforms is a list of transformations applied on the 'raw' dataset before the data is fed to the network.
+# Here, Data augmentation (RandomCrop and Horizontal Flip) are applied to each batch, differently at each epoch, on the training set data only
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize_scratch,
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    normalize_scratch,
+])
+
+### The data from CIFAR10 are already downloaded in the following folder
+rootdir = '/opt/img/effdl-cifar10/'
+# rootdir = './effdl-cifar10/'
+
+c10train = CIFAR10(rootdir,train=True,download=True,transform=transform_train)
+c10test = CIFAR10(rootdir,train=False,download=True,transform=transform_test)
+
+trainloader = DataLoader(c10train,batch_size=32,shuffle=True)
+testloader = DataLoader(c10test,batch_size=32)
+
+num_train_examples = len(c10train)
+num_samples_subset = 15000
+
+## We set a seed manually so as to reproduce the results easily
+# seed  = 2147483647
+
+## Generate a list of shuffled indices ; with the fixed seed, the permutation will always be the same, for reproducibility
+# indices = list(range(num_train_examples))
+# np.random.RandomState(seed=seed).shuffle(indices)## modifies the list in place
+
+## We define the Subset using the generated indices
+# c10train_subset = torch.utils.data.Subset(c10train,indices[:num_samples_subset])
+print(f"Initial CIFAR10 dataset has {len(c10train)} samples")
+# print(f"Subset of CIFAR10 dataset has {len(c10train_subset)} samples")
+
+# Finally we can define anoter dataloader for the training data
+# trainloader_subset = DataLoader(c10train_subset,batch_size=32,shuffle=True)
+### You can now use either trainloader (full CIFAR10) or trainloader_subset (subset of CIFAR10) to train your networks.
+
+# Évaluation
+
+def evaluate(net, dataloader):
+    net.eval()
+    correct, total, test_loss = 0, 0, 0.0
+    criterion = nn.CrossEntropyLoss()
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    acc = 100. * correct / total
+    return test_loss / len(dataloader), acc
+
+
+
+
+# Trainning
+
+# Une boucle simple d'entraînement
+def train(net, trainloader, testloader, optimizer, scheduler, epochs=10):
+    criterion = nn.CrossEntropyLoss()
+    for epoch in range(epochs):
+        net.train()
+        total, correct, loss_total = 0, 0, 0.0
+        for inputs, labels in trainloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            loss_total += loss.item()
+
+        train_acc = 100. * correct / total
+        test_loss, test_acc = evaluate(net, testloader)
+
+        print(f"[Epoch {epoch+1:2d}] LR: {scheduler.get_last_lr()[0]:.5f} | "
+              f"Train Loss: {loss_total:.3f} | Train Acc: {train_acc:.2f}% | "
+              f"Test Loss: {test_loss:.3f} | Test Acc: {test_acc:.2f}%")
+
+        scheduler.step()
+
+# Appel d'entraînement
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net = VGG('VGG11').to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    scheduler = StepLR(optimizer, step_size=20, gamma=0.5)
+    
+    train(net, trainloader, testloader, optimizer, scheduler, epochs=20)
